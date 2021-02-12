beb8ac6b185928e18a39172417b5f9eedd95a54e
Guru Prasad G <guru.prasad@thoughtworks.com>
2021-02-12 21:11:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# WEB Dev Course - NodeJS Boilerplate
+
+## Prerequisites
+
+1. NodeJS (https://nodejs.org/en/)
+1. MongoDB (https://docs.mongodb.com/manual/administration/install-community/)
+
+## Instructions to run locally
+
+1. Clone the repo: `git clone https://github.com/gpstrange/backend-demo.git`
+1. `cd backend-demo`
+1. Install packages: `npm i`
+1. Launch: `npm start`
+1. Visit in your browser at: `http://localhost:3000`
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost:27017/studenttest', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Connected to mongo');
+}).catch((err) => {
+    console.log('MongoDB connection error. Please make sure MongoDB is running. ');
+    process.exit();
+});
+
+app.use((req, res, next) => {
+    return res.json({status: "Hey there"});
+});
+
+app.listen(3000, () => {
+    console.log('Server listening in port 3000');
+});

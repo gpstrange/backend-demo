5a28d1c8fb53bcb3cb6d2473c59fe698bb760102
Guru Prasad G <guru.prasad@thoughtworks.com>
2021-02-17 20:37:27 +0530

    Added ejs

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true }));
 
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 mongoose.connect('mongodb://localhost:27017/studenttest', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-    console.log('Connected to mongo');
+    console.log('Connected to mongodb');
 }).catch((err) => {
     console.log('MongoDB connection error. Please make sure MongoDB is running. ');
     process.exit();
 });
 
 app.get('/', (req, res, next) => {
-    return res.json({status: "Hey there"});
+    return res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// app.get('/home', (req, res, next) => {
+//     const names = ["guru", "sanjay", "hari", "kavya"];
+//     return res.render('math', { students: names });
+// });
+
+app.get('/login', (req, res, next) => {
+    return res.render('login');
 });
 
 app.listen(3000, () => {
